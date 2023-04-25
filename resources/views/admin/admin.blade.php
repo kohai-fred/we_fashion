cd0b1048993ddd4170d927246e6c8338c5eeb369
kohai-fred <frederick.vu.pro@gmail.com>
2023-04-25 16:24:56 +0200

    feat: add TomSelect (multiselect lib)

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/tom-select@2.2.2/dist/css/tom-select.bootstrap5.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/tom-select@2.2.2/dist/js/tom-select.complete.min.js"></script>
     <title>@yield('title')</title>
 </head>
 <body>
@@ -16,7 +19,7 @@
     @include('shared.navbar')
 
     @if (!str_contains($route, 'admin.product.index'))
-        <a href="./" class="m-3 btn btn-outline-primary btn-sm rounded-circle">
+        <a href="{{ route('admin.product.index') }}" class="m-3 btn btn-outline-primary btn-sm rounded-circle">
             <i class="bi bi-arrow-return-left"></i>
         </a>
     @endif
@@ -31,6 +34,8 @@
 
         @yield('content')
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script>
+        new TomSelect('select[multiple]',{plugins:{remove_button:{title: 'Supprimer'}}})
+    </script>
 </body>
 </html>
