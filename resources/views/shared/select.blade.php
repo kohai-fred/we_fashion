c35d44f2deeaf002a47197cf1e79484266c93938
kohai-fred <frederick.vu.pro@gmail.com>
2023-04-26 17:12:20 +0200

    bugfix: add class error

--- resources/views/shared/select.blade.php
+++ resources/views/shared/select.blade.php
@@ -7,7 +7,7 @@
 
 <div @class(["form-group", $class])>
     <label for="{{ $name }}">{{ $label }}</label>
-    <select name="{{ $name }}[]" id="{{ $name }}" multiple>
+    <select class="form-control @error($name) is-invalid @enderror" name="{{ $name }}[]" id="{{ $name }}" multiple>
         @foreach ($options as $k => $v)
             <option @selected($value->contains($k)) value="{{ $k }}">{{ $v }}</option>
         @endforeach
