85cbf1e702309825ca1f43305363eafd42827b46
kohai-fred <frederick.vu.pro@gmail.com>
2023-04-27 18:38:33 +0200

    feat: get categories

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $products = Product::orderBy('created_at', 'desc')->paginate(6);
-        return view('home', ['products' => $products]);
+        return view('home', [
+            'products' => $products,
+            'categories' => Category::pluck('name', 'id')
+        ]);
     }
 }
