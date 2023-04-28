36d9de66c77b7b70af00ea42516dbe41fe133caa
kohai-fred <frederick.vu.pro@gmail.com>
2023-04-28 23:31:02 +0200

    feat: add img product seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -5,8 +5,11 @@ namespace Database\Seeders;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\Size;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Http\UploadedFile;
+
 
 class ProductSeeder extends Seeder
 {
@@ -15,7 +18,7 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        $products = Product::factory(5)->create();
+        $products = Product::factory(80)->create();
         $categories = Category::pluck('id')->toArray();
         $sizes = Size::pluck('id')->toArray();
 
@@ -30,6 +33,22 @@ class ProductSeeder extends Seeder
                 $randSize[] = $sizes[$i];
             }
             $product->sizes()->sync($randSize);
+
+            // Check product category and save random image
+            $category = Category::find($categories[$categoryId]);
+            if ($category->name == 'homme') {
+                $imagesArray = 'images_seeder/hommes';
+            } else {
+                $imagesArray = 'images_seeder/femmes';
+            }
+
+            $imageFiles = public_path($imagesArray);
+            $chemin = join(DIRECTORY_SEPARATOR, [base_path(), $imagesArray, '*']);
+            $imageFiles = File::glob($chemin);
+            $imagePath = $imageFiles[array_rand($imageFiles)];
+            $image = new UploadedFile($imagePath, 'product.jpg', 'image/jpeg', null, true);
+            $product['image'] = $image->store('product', 'public');
+            $product->update($product->getAttributes());
         }
     }
 }
