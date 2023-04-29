117753a0d34210db33ed6eb8e5146c1849007f53
kohai-fred <frederick.vu.pro@gmail.com>
2023-04-29 12:01:03 +0200

    bugfix: remove duplicate TomSelect

--- resources/views/admin/baseAdmin.blade.php
+++ resources/views/admin/baseAdmin.blade.php
@@ -1,11 +1,5 @@
 @extends('layouts.app')
 
-@section('head')
-    {{-- Tom Select - for multiselect --}}
-    <link href="https://cdn.jsdelivr.net/npm/tom-select@2.2.2/dist/css/tom-select.bootstrap5.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/tom-select@2.2.2/dist/js/tom-select.complete.min.js"></script>
-@endsection
-
 @section('base-title', 'Admin')
 
 @section('navbar')
@@ -30,14 +24,4 @@
 
     </div>
 
-@endsection
-
-@section('script')
-    {{-- Tom Select - catch all multiselect --}}
-    <script>
-        const selectsMultipleEl = Array.from(document.querySelectorAll('select[multiple]'))
-        selectsMultipleEl.forEach(select => {
-            new TomSelect(`#${select.id}`,{plugins:{remove_button:{title: 'Supprimer'}}})
-        });
-    </script>
 @endsection
